679d4e7da0a50597795cde8d10203c3d8ccc770c
TUDOR ODOBASA <odobasa.tudor174169@student.ubbcluj.ro>
2022-01-04 14:26:08 +0200

    Acesta este laboratorul 12

--- Odobasa_Tudor_Lap10/ListPage.xaml.cs
+++ Odobasa_Tudor_Lap10/ListPage.xaml.cs
@@ -30,5 +30,12 @@ namespace Odobasa_Tudor_Lap10
             await App.Database.DeleteShopListAsync(slist);
             await Navigation.PopAsync();
         }
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+        }
     }
 }
